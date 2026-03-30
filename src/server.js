93bea2980ddaffa7ab9213a4c6e2b3e3009d1b84
EA - Lívia De Souza Silva <livia_souza01@educadventista.org>
2026-03-30 09:21:11 -0300

    Atualiza api; commita mudanças locais

--- src/server.js
+++ src/server.js
@@ -4,12 +4,11 @@ import { tarefas } from "./dados.js";
 const app = express();
 const PORTA = 3000;
 
-// Permitir receber JSON no corpo da requisição
+// Middleware
 app.use(express.json());
 
 /*
 GET /tarefas
-Lista todas as tarefas
 */
 app.get("/tarefas", (req, res) => {
   res.status(200).json(tarefas);
@@ -17,12 +16,10 @@ app.get("/tarefas", (req, res) => {
 
 /*
 POST /tarefas
-Cria uma nova tarefa
 */
 app.post("/tarefas", (req, res) => {
   const { titulo } = req.body;
 
-  // Validação mínima
   if (!titulo || titulo.trim() === "") {
     return res.status(400).json({
       erro: "Título é obrigatório."
@@ -40,6 +37,43 @@ app.post("/tarefas", (req, res) => {
   res.status(201).json(novaTarefa);
 });
 
+/*
+PATCH /tarefas/:id
+*/
+app.patch("/tarefas/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const { titulo, concluida } = req.body;
+
+  const tarefa = tarefas.find(t => t.id === id);
+
+  if (!tarefa) {
+    return res.status(404).json({ erro: "Tarefa não encontrada" });
+  }
+
+  if (titulo !== undefined) tarefa.titulo = titulo;
+  if (concluida !== undefined) tarefa.concluida = concluida;
+
+  res.json(tarefa);
+});
+
+/*
+DELETE /tarefas/:id
+*/
+app.delete("/tarefas/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const index = tarefas.findIndex(t => t.id === id);
+
+  if (index === -1) {
+    return res.status(404).json({ erro: "Tarefa não encontrada" });
+  }
+
+  tarefas.splice(index, 1);
+
+  res.status(204).send();
+});
+
+// START
 app.listen(PORTA, () => {
   console.log(`Servidor rodando em http://localhost:${PORTA}`);
 });
